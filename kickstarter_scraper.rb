49fb06fd9e3f0269b29f49c4f2f3004e1cba2fd0
Emily Nicholl Liepe <e.nicholl.liepe@gmail.com>
2019-02-23 00:39:03 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,6 +7,13 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   
   kickstarter = Nokogiri::HTML(html)
+  
+  projects = {}
+  
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end
+  projects 
   binding.pry 
 end
 
