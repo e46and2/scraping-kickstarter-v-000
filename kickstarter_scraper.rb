68dc85139ab0299eb8c5800ed4101e34014edf97
Emily Nicholl Liepe <e.nicholl.liepe@gmail.com>
2019-02-23 00:34:59 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,6 +14,7 @@ end
 #title: project.css("h2.bbcard_name strong a").text
 #image link: project.css("div.project-thumbnail a img").attribute("src").value
 #project location: project.css(".location-name").text
+# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 
 
 create_project_hash
